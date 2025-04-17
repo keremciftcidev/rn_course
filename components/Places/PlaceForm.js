01895505b6472c21320d1cce5b6d9c31819a122c
Kerem Ciftci <krmciftci17@gmail.com>
2025-04-17 23:45:18 +0300

    Adding an "Add Place" Screen + Navigation

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,3 +1,5 @@
+import { Text, View } from "react-native";
+
 function PlaceForm() {
     return <View>
         <Text>The Place Form </Text>
--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,5 +1,6 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import PlaceItem from "./PlaceItem";
+import { Colors } from "../../constants/colors";
 
 function PlacesList({ places }) {
 
@@ -31,6 +32,7 @@ const styles = StyleSheet.create({
     },
     fallbackText:{
         fontSize:16,
+        color:Colors.primary200
 
     }
 })
