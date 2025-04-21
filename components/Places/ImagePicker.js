4975fe15233f2a12bfa868a00707c2d584e8d1e3
Kerem Ciftci <krmciftci17@gmail.com>
2025-04-22 01:11:14 +0300

    Showing an Image Preview

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,44 +1,69 @@
-import { Alert, Button, View } from "react-native";
-import {launchCameraAsync, useCameraPermissions,PermissionStatus} from "expo-image-picker"
+import { Alert, Button, Image, View,Text, StyleSheet } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+import { useState } from "react";
+import { Colors } from "../../constants/colors";
 
 function ImagePicker() {
-    const  [cameraPermissionInformation,requestPermission]= useCameraPermissions()
+  const [pickedImage, setPickedImage] = useState();
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
 
-    async function verifyPermission(){
-        if(cameraPermissionInformation.status === PermissionStatus.UNDETERMINED){
-            const permissionResponse = await requestPermission()
-            return permissionResponse.granted
-        }
-
-        if(cameraPermissionInformation.status === PermissionStatus.DENIED){
-            Alert.alert(
-                "İzin gerekli",
-                "Kamera erişimi için izin vermen gerekiyor. Ayarlardan kontrol edebilirsin."
-            )
-            return false
-        }
-        return true
-
+  async function verifyPermission() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
     }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "İzin gerekli",
+        "Kamera erişimi için izin vermen gerekiyor. Ayarlardan kontrol edebilirsin."
+      );
+      return false;
+    }
+    return true;
+  }
   async function takeImageHandler() {
-    const hasPermission = await verifyPermission()
+    const hasPermission = await verifyPermission();
 
-    if(!hasPermission ){
-        return
+    if (!hasPermission) {
+      return;
     }
-   const image =  await launchCameraAsync({
-    allowsEditing:true,
-    aspect:[16,9],
-    quality:0.5
-   })
-   console.log(image)
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    setPickedImage(image.uri);
+  }
+
+  let imagePreview = <Text >No image taken yet</Text>;
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
   }
   return (
-    <View>
-      <View></View>
+    <View style={styles.imagePreview}>
+      <View >{imagePreview}</View>
       <Button title="take image" onPress={takeImageHandler} />
     </View>
   );
 }
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+    imagePreview: {
+        width: "90%",               // ← ekranın %90’ı kadar genişlik
+        alignSelf: "center",        // ← ortaya hizalama
+        height: 200,
+        marginVertical: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: Colors.primary100,
+        borderRadius: 4,
+      },
+});
