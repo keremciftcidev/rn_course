0658706e57b8d45f55e323c32087101beb10bc6f
Kerem Ciftci <krmciftci17@gmail.com>
2025-05-06 02:56:57 +0300

    SQLite: Getting Started & Initialization

---
+++ util/database.js
@@ -0,0 +1,30 @@
+import * as SQLite from "expo-sqlite";
+
+// Veritabanını açıyoruz
+const database = SQLite.openDatabaseSync("places.db");
+
+// Tablomuzu oluşturuyoruz (varsa yeniden oluşturmaz)
+export function init() {
+ const promise  = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS places (
+          id INTEGER PRIMARY KEY NOT NULL,
+          title TEXT NOT NULL,
+          imageUri TEXT NOT NULL,
+          address TEXT NOT NULL,
+          lat REAL NOT NULL,
+          lng REAL NOT NULL
+        );`,
+        [],
+        () => {
+          resolve(); // başarılıysa
+        },
+        (_, error) => {
+          reject(error); // hata varsa
+        }
+      );
+    });
+  });
+  return promise
+}
